59e6e61896ae375a65d79bd74b2ef0f67ad77c3f
Vladislav Mishukov <vladg3@mail.ru>
2023-06-28 18:33:21 +0700

    Добавил функционал на main сториборд: Увеличить и уменьшить значение

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private var counter: Int = 0
+    
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.adjustsFontSizeToFitWidth = true
+        counterLabel.minimumScaleFactor = 0.5
+        counterLabel.numberOfLines = 1
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonDecrease(_ sender: Any) {
+        if counter > 0 {
+            counter -= 1
+            counterLabel.text = "Значение счетчика: \(counter)"
+        }
+    }
+    
+    @IBAction func buttonIncrease(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
 }
 
